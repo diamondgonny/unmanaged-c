957b3edb3787265da17a3265c20a139b9f18c9f7
Ernest Kim <diamondgonny@gmail.com>
2022-05-26 22:13:45 +0900

    Lab4 hustle (pre-draft)

---
+++ Lab4/my_string.c
@@ -0,0 +1,93 @@
+#include "my_string.h"
+#define TRUE (1)
+#define FALSE (0)
+
+void swap(char* a, char* b)
+{
+    char temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+size_t get_str_length(char* str) /* const? */
+{
+    char* p = str;
+
+    while (*p++ != '\0') {
+    }
+    return p - str - 1;
+}
+
+size_t cmp_str(const char* str, const char* word)
+{
+    while (*word != '\0' && *str == *word) {
+        ++str;
+        ++word;
+    }
+    *word == '\0' ? return TRUE : return FALSE;
+}
+
+void reverse(char* str)
+{
+    size_t i;
+    size_t str_length = get_str_length(str);
+
+    for (i = 0; i < (str_length / 2); ++i) {
+        swap(str + i, str_length - 1 - i);
+    }
+}
+
+int index_of(const char* str, const char* word)
+{
+    size_t i;
+    size_t result = 0;
+    size_t str_length = get_str_length(str);
+    size_t word_length = get_str_length(word);
+
+    for (i = 0; i < str_length - word_length + 1; ++i) {
+        result = cmp_str(str, word);
+        result == TRUE ? return i : ++str;
+    }
+    return -1;
+}
+
+void reverse_by_words(char* str);
+
+char* tokenize(char* str_or_null, const char* delims)
+{
+    static char* p_ongoing;
+    const char* p_delims = delims;
+
+    /* [반환값] 스타또 포인트 (동기화 : 새로하기 vs 이어하기) */
+    str_or_null != NULL ? p_ongoing = str_or_null : str_or_null = p_ongoing;
+
+    /* 현재 위치 '\0'? NULL을 반환 */
+    if (*p_ongoing == '\0') {
+        return NULL;
+    }
+
+    /* 현재 위치 구분자? 구분자이면 루프돌고(좌항), 비구분자이면 탈출(우항). */
+    /* 예시)  ...'g'g..................,,,0  */
+    while (*p_ongoing != '\0') {
+        while (*p_delims != '\0') {
+            *p_ongoing == *p_delims ? break : ++p_delims;
+        }
+        *p_delims != '\0' ? ++p_ongoing : break;
+    }
+
+    /* 유효한 토큰 반환을 위한 구분자 감지 알고리즘 */
+    str_or_null = p_ongoing;
+    for (p_ongoing = str_or_null; *p_ongoing != NULL; ++p_ongoing) {
+        for (p_delims = delims; *p_delims != NULL; ++p_delims) {
+            if (*p_ongoing == *p_delims) {
+                *p_ongoing++ = '\0';
+                return str_or_null;
+            }
+        }
+    }
+    return str_or_null;
+}
+
+char* reverse_tokenize(char* str_or_null, const char* delims);
