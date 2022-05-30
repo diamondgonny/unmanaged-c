6d92ff4bb989ee3480c145561a9ff7911befc2a7
Ernest Kim <diamondgonny@gmail.com>
2022-05-30 16:13:13 +0900

    Lab5 init

---
+++ Lab5/receipter.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "receipter.h"
+
+int add_item(const char* name, double price)
+{
+
+}
+
+void set_tip(double tip)
+{
+
+}
+
+void set_message(const char* message)
+{
+
+}
+
+int print_receipt(const char* filename, time_t timestamp)
+{
+
+}
