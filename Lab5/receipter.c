e9e36ef30e6724bc26a3206e58ba922b6bc2fda0
Ernest Kim <diamondgonny@gmail.com>
2022-06-01 10:11:57 +0900

    Lab5 draft (before file i/o)

--- Lab5/receipter.c
+++ Lab5/receipter.c
@@ -7,15 +7,23 @@
 #define MESSAGE_LENGTH (75)
 
 static size_t order_number = 0;
-static char foodname[FOODAMOUNT_MAX][FOODNAME_LENGTH];
+static char foodname[FOODAMOUNT_MAX][FOODNAME_LENGTH + 1]; /* '\0' */
 static double foodprice[FOODAMOUNT_MAX];
 static size_t food_counter = 0;
 static double tip_thanks = 0;
-static char message_thanks[MESSAGE_LENGTH];
+static char message_thanks[MESSAGE_LENGTH + 1];
 
-void cpy_string(int length)
+void my_strncpy(char* str, const char* input, size_t length)
 {
     /* 문자열을 복사해서 붙여넣고, 끝에 '\0'을 넣어주자. length 넘어가면 짤림) */
+    while (*input != '\0' && length != 0)
+    {
+        *str = *input;
+        ++str;
+        ++input;
+        --length;
+    }
+    *str = '\0';
 }
 
 int add_item(const char* name, double price)
@@ -23,7 +31,7 @@ int add_item(const char* name, double price)
     if (food_counter < FOODAMOUNT_MAX) {
         /* 2차원 배열에서의 포인터 : *(foodname + food_counter) */
         /* sscanf로 입력하면, 구분자(공백문자) 버림... */
-
+        my_strncpy(*(foodname + food_counter), name, FOODNAME_LENGTH);
         foodprice[food_counter] = price;
         ++food_counter;
     }
@@ -42,6 +50,7 @@ void set_tip(double tip)
 void set_message(const char* message)
 {
     /* sscanf로 입력하면, 구분자(공백문자) 버림... */
+    my_strncpy(message_thanks, message, MESSAGE_LENGTH);
 }
 
 int print_receipt(time_t timestamp)
@@ -63,25 +72,26 @@ int print_receipt(time_t timestamp)
     printf("Charles' Seafood\n");
     printf("--------------------------------------------------\n");
     printf("%04d-%02d-%02d %02d:%02d:%02d", t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec);
+    printf("                          ");
     printf("%05lu\n", order_number);
     printf("--------------------------------------------------\n");
     for (i = 0; i < FOODAMOUNT_MAX; ++i) {
-        printf("%s", foodname[i]);
-        printf("%.2f\n", foodprice[i]);
+        printf("%33s", foodname[i]);
+        printf("%17.2f\n", foodprice[i]);
     }
     printf("\n");
-    printf("%20s", "Subtotal");
-    printf("%.2f\n", subtotal);
-    printf("%20s", "Tip");
-    printf("%.2f\n", tip_thanks);
-    printf("%20s", "Tax");
-    printf("%.2f\n", subtotal * 0.05);
-    printf("%20s", "Total");
-    printf("%.2f\n", subtotal + tip_thanks + subtotal * 0.05);
+    printf("%33s", "Subtotal");
+    printf("%17.2f\n", subtotal);
+    printf("%33s", "Tip");
+    printf("%17.2f\n", tip_thanks);
+    printf("%33s", "Tax");
+    printf("%17.2f\n", subtotal * 0.05);
+    printf("%33s", "Total");
+    printf("%17.2f\n", subtotal + tip_thanks + subtotal * 0.05);
     printf("\n");
     printf("%s\n", message_thanks); /* it might be line unit */
     printf("==================================================\n");
-    printf("%20s\n", "Tax#-51234");
+    printf("%50s\n", "Tax#-51234");
     printf("\n");
 
     ++order_number;
