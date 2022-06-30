50d83ba573d7519e4d0e9e44e21f46adb5eb9793
Ernest Kim <diamondgonny@gmail.com>
2022-06-30 19:41:46 +0900

    Assignment3 init

---
+++ Assignment3/document_analyzer.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "document_analyzer.h"
+
+int load_document(const char* document);
+
+void dispose(void);
+
+unsigned int get_total_word_count(void);
+
+unsigned int get_total_sentence_count(void);
+
+unsigned int get_total_paragraph_count(void);
+
+const char*** get_paragraph_or_null(const unsigned int paragraph_index);
+
+unsigned int get_paragraph_word_count(const char*** paragraph);
+
+unsigned int get_paragraph_sentence_count(const char*** paragraph);
+
+const char** get_sentence_or_null(const unsigned int paragraph_index, const unsigned int sentence_index);
+
+unsigned int get_sentence_word_count(const char** sentence);
+
+int print_as_tree(const char* filename);
