1faef60f30eb1f14ee41506383bdf50f2c7381f4
Ernest Kim <diamondgonny@gmail.com>
2022-07-07 21:20:24 +0900

    Lab8 modified : refactoring (3)

--- Lab8/parentheses.c
+++ Lab8/parentheses.c
@@ -3,11 +3,14 @@
 
 size_t find_matching_parentheses(ringbuffer_t* ringbuffer, const char* str)
 {
-    const char* p_str_start = str;
+    const char* p_rparen = str;
     size_t len = 0;
 
-    while (*p_str_start++ != '\0') {
-        len = enqueue_parentheses(ringbuffer, str, p_str_start, len);
+    while (*p_rparen != '\0') {
+        if (*p_rparen == ')' || *p_rparen == '}' || *p_rparen == ']' || *p_rparen == '>') {
+            len = enqueue_parentheses(ringbuffer, str, p_rparen, len);
+        }
+        ++p_rparen;
     }
 
     if (len > ringbuffer->max_size) {
@@ -19,34 +22,28 @@ size_t find_matching_parentheses(ringbuffer_t* ringbuffer, const char* str)
     return len;
 }
 
-size_t enqueue_parentheses(ringbuffer_t* ringbuffer, const char* str, const char* p_str_start, size_t len)
+size_t enqueue_parentheses(ringbuffer_t* ringbuffer, const char* str, const char* p_rparen, size_t len)
 {
-    const char* p_str = p_str_start;
-    char right_pair = *p_str_start;
-    char left_pair;
+    const char* p = p_rparen;
+    char rparen = *p_rparen;
+    char lparen;
     size_t paren_matcher = 0;
 
-    /* note : ASCII code */
-    if (*p_str == ')') {
-        left_pair = right_pair - 1;
-    } else if (*p_str == '}' || *p_str == ']' || *p_str == '>') {
-        left_pair = right_pair - 2;
-    } else {
-        return len;
-    }
+    /* note : function works as if only ')}]>' have accepted, and ASCII code was referred  */
+    *p_rparen == ')' ? lparen = rparen - 1 : (lparen = rparen - 2);
 
-    for (; p_str >= str; --p_str) {
-        if (p_str == p_str_start) {
+    for (; p >= str; --p) {
+        if (p == p_rparen) {
             continue;
         }
-        if (*p_str == right_pair) {
+        if (*p == rparen) {
             ++paren_matcher;
         }
-        if (*p_str == left_pair) {
+        if (*p == lparen) {
             if (paren_matcher == 0) {
                 size_t ring_index = len % ringbuffer->max_size;
-                ringbuffer->parentheses[ring_index].closing_index = p_str_start - str;
-                ringbuffer->parentheses[ring_index].opening_index = p_str - str;
+                ringbuffer->parentheses[ring_index].closing_index = p_rparen - str;
+                ringbuffer->parentheses[ring_index].opening_index = p - str;
                 ++len;
                 break;
             } else {
@@ -57,9 +54,15 @@ size_t enqueue_parentheses(ringbuffer_t* ringbuffer, const char* str, const char
 
     return len;
 }
-/* how the function 'enqueue parentheses' works on */
-/*(str)    <------ ↓p_str --------    (↓p_str_start)    */
-/* ( ( ( ] { { { < > < > < > [ m o r e } } { } } ) '\0' */
 
-/*(parentheses[ring_index]) */
-/* V V V ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */
+/* how the while loop in 'find_matching...' works on... */
+/* (str)          ----------- ↓p_rparen -------->        */
+/*  ( ( ( ] { { { < > < > < > [ m o r e } } { } } ) '\0'  */
+
+/* how the function 'enqueue parentheses' works on...   */
+/* (str)          <------ ↓p --------  (↓p_rparen)       */
+/*  ( ( ( ] { { { < > < > < > [ m o r e } } { } } ) '\0'  */
+
+/* 'ring buffer'                      */
+/* (parentheses[ring_index])           */
+/*  V V V ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ  */
