821644b2c475aeebfc91d90ab0d607503fbb78a3
Ernest Kim <diamondgonny@gmail.com>
2022-07-07 10:45:53 +0900

    Assignment3 modified : rest of files

--- Assignment3/main.c
+++ Assignment3/main.c
@@ -2,6 +2,81 @@
 #include <string.h>
 #include "document_analyzer.h"
 
+void test_empty(void)
+{
+    assert(load_document("input_empty.txt") == TRUE);
+    assert(print_as_tree("output_empty.txt") == FALSE);
+    assert(get_total_word_count() == 0U);
+    assert(get_total_sentence_count() == 0U);
+    assert(get_total_paragraph_count() == 0U);
+ 
+    dispose();
+ 
+    puts("test_empty");
+}
+ 
+void test_document(void)
+{
+    const char** sentence = NULL;
+    const char*** paragraph = NULL;
+    assert(get_paragraph_or_null(0) == NULL);
+    assert(get_sentence_or_null(0, 0) == NULL);
+    assert(get_total_word_count() == 0U);
+    assert(get_total_sentence_count() == 0U);
+    assert(get_total_paragraph_count() == 0U);
+    assert(print_as_tree("output_document.txt") == FALSE);
+ 
+    assert(load_document("document.txt") == TRUE);
+ 
+    assert(get_total_word_count() == 62U);
+    assert(get_total_sentence_count() == 6U);
+    assert(get_total_paragraph_count() == 3U);
+ 
+    paragraph = get_paragraph_or_null(0);
+    assert(paragraph != NULL);
+    assert(get_paragraph_word_count(paragraph) == 19);
+    assert(get_paragraph_sentence_count(paragraph) == 1);
+    sentence = get_sentence_or_null(0, 0);
+    assert(sentence != NULL);
+    assert(get_sentence_word_count(sentence) == 19);
+    assert(get_sentence_or_null(0, 1) == NULL);
+ 
+    paragraph = get_paragraph_or_null(1);
+    assert(paragraph != NULL);
+    assert(get_paragraph_word_count(paragraph) == 28);
+    assert(get_paragraph_sentence_count(paragraph) == 3);
+    sentence = get_sentence_or_null(1, 0);
+    assert(sentence != NULL);
+    assert(get_sentence_word_count(sentence) == 10);
+    sentence = get_sentence_or_null(1, 1);
+    assert(sentence != NULL);
+    assert(get_sentence_word_count(sentence) == 9);
+    sentence = get_sentence_or_null(1, 2);
+    assert(sentence != NULL);
+    assert(get_sentence_word_count(sentence) == 9);
+    assert(get_sentence_or_null(1, 3) == NULL);
+ 
+    paragraph = get_paragraph_or_null(2);
+    assert(paragraph != NULL);
+    assert(get_paragraph_word_count(paragraph) == 15);
+    assert(get_paragraph_sentence_count(paragraph) == 2);
+    sentence = get_sentence_or_null(2, 0);
+    assert(sentence != NULL);
+    assert(get_sentence_word_count(sentence) == 8);
+    sentence = get_sentence_or_null(2, 1);
+    assert(sentence != NULL);
+    assert(get_sentence_word_count(sentence) == 7);
+    assert(get_sentence_or_null(2, 2) == NULL);
+ 
+    assert(get_paragraph_or_null(3) == NULL);
+ 
+    assert(print_as_tree("output_document.txt") == TRUE);
+ 
+    dispose();
+ 
+    puts("test_document");
+}
+
 int main(void)
 {
     const char** sentence = NULL;
@@ -55,6 +130,9 @@ int main(void)
     assert(print_as_tree("output.txt") == TRUE);
 
     dispose();
+
+    test_empty();
+    test_document();
 
     return 0;
 }
