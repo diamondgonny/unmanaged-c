5d8222616cd89d4494fabbb358d70ec04a0ac190
Ernest Kim <diamondgonny@gmail.com>
2022-07-13 08:55:12 +0900

    Lab9 modified : about expression

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -7,13 +7,13 @@
 
 #define ABS(x) (((x) >= (0)) ? (x) : -(x))
 
-#define RANGE(curr, min, max) for (curr = min; curr <= max; curr++)
+#define RANGE(curr, min, max) for (curr = min; curr <= max; (curr)++)
 
-#define RANGE_DESC(curr, max, min) for (curr = max; curr >= min; curr--)
+#define RANGE_DESC(curr, max, min) for (curr = max; curr >= min; (curr)--)
 
 #define SET(ary, start, count, value)   do { \
                                             int set_i; \
-                                            for (set_i = start; set_i < start + count; set_i++) { \
+                                            for (set_i = start; set_i < start + count; (set_i)++) { \
                                                 ary[set_i] = value; \
                                             } \
                                         } while (0) \
