8935c403fb3122e0f6ea1cbe6a047fda13c2456a
Ernest Kim <diamondgonny@gmail.com>
2022-07-12 15:37:25 +0900

    Lab9 retry

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -7,15 +7,15 @@
 
 #define ABS(x) (((x) >= (0)) ? (x) : -(x))
 
-#define RANGE(curr, min, max) for (curr = min; curr <= max; curr++) \
+#define RANGE(curr, min, max) for (curr = min; curr <= max; curr++)
 
-#define RANGE_DESC(curr, max, min) for (curr = max; curr >= min; curr--) \
+#define RANGE_DESC(curr, max, min) for (curr = max; curr >= min; curr--)
 
 #define SET(ary, start, count, value)   { \
                                         int set_i; \
                                         for (set_i = start; set_i < start + count; set_i++) \
                                         *(ary + set_i) = value; \
                                         } \
-                                        if(1) 1\
+                                        if (1) 1
 
 #endif /* MACROS_H */
