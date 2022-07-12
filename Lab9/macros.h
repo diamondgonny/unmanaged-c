449728ee031b5b878a475b475283445b5c6470bd
Ernest Kim <diamondgonny@gmail.com>
2022-07-12 17:07:31 +0900

    Lab9 retry

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -11,11 +11,11 @@
 
 #define RANGE_DESC(curr, max, min) for (curr = max; curr >= min; curr--)
 
-#define SET(ary, start, count, value)   { \
-                                        int set_i; \
-                                        for (set_i = start; set_i < start + count; set_i++) \
-                                        *(ary + set_i) = value; \
-                                        } \
-                                        if (1) 1
+#define SET(ary, start, count, value)   do { \
+                                            int set_i; \
+                                            for (set_i = start; set_i < start + count; set_i++) { \
+                                                *(ary + set_i) = value; \
+                                            } \
+                                        } while (0) \
 
 #endif /* MACROS_H */
