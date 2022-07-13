1cedbfb3314ab802670e2a7aadee9e7855317b01
Ernest Kim <diamondgonny@gmail.com>
2022-07-13 11:42:04 +0900

    Lab9 modified : about operation (ary : array -> pointer)

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -16,7 +16,7 @@
 #define SET(ary, start, count, value)   do { \
                                             size_t set_i; \
                                             for ((set_i) = (start); (set_i) < ((start) + (count)); (set_i)++) { \
-                                                ary[(set_i)] = (value); \
+                                                *((ary) + (set_i)) = (value); \
                                             } \
                                         } while (0) \
 
