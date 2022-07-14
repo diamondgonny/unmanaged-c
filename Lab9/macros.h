503aeacf87de29c1dec9fa32e76afcde7665cf0d
Ernest Kim <diamondgonny@gmail.com>
2022-07-14 09:12:46 +0900

    Lab9 modified : removed round brack of 'ary' (testing)

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -16,9 +16,8 @@
 #define SET(ary, start, count, value)   do { \
                                             size_t set_i; \
                                             for (set_i = (start); set_i < ((start) + (count)); set_i++) { \
-                                                *((ary) + set_i) = (value); \
+                                                *(ary + set_i) = (value); \
                                             } \
                                         } while (0) \
-
 
 #endif /* MACROS_H */
