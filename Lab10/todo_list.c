a2dad584126610807e90e0858f584de11ce982d5
Ernest Kim <diamondgonny@gmail.com>
2022-07-18 21:30:40 +0900

    Lab10 init

---
+++ Lab10/todo_list.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "todo_list.h"
+
+todo_list_t init_todo_list(size_t max_size);
+
+void finalize_todo_list(todo_list_t* todo_list);
+
+bool add_todo(todo_list_t* todo_list, const int32_t priority, const char* task);
+
+bool complete_todo(todo_list_t* todo_list);
+
+const char* peek_or_null(const todo_list_t* todo_list);
+
+size_t get_count(const todo_list_t* todo_list);
+
+bool is_empty(const todo_list_t* todo_list);
