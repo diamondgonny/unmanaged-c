3889b4193628c69df93ac89fe48012dde00dd6d8
Ernest Kim <diamondgonny@gmail.com>
2022-07-21 11:48:01 +0900

    Lab10 modified : changed comments

--- Lab10/todo_list.c
+++ Lab10/todo_list.c
@@ -51,8 +51,9 @@ bool add_todo(todo_list_t* todo_list, const int32_t priority, const char* task)
 {
     index_t index = get_index_for_add(todo_list, priority);
     index_t tmp = todo_list->head;
-    index_t* p = &todo_list->head; // 첫 번째 노드를 가리키는 index의 주소 (예외 : INT_MIN)
-    index_t* next_p; // 두 번째 노드를 가리키는 index의 주소 (예외 : INT_MIN)
+    index_t* p = &todo_list->head; // 최전방 노드 index의 주소에서부터 ~ (예외 : INT_MIN)
+    index_t* next_p; // 최전방 다음 노드 index의 주소에서부터 ~ (예외 : INT_MIN)
+    /* Note : todo_list.node에 (node_t)배열로 할당된 메모리 안에서, 연결리스트와 같은 구조를 가짐 */
 
     //  노드가 꽉찼을 때 혹은 priority가 음수일 때의 false 반환
     if (index == INT_MIN || priority < 0) {
