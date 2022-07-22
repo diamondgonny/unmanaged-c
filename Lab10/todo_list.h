51ae88f93a409d349ffd4881d0f8eae6406e1bc0
Ernest Kim <diamondgonny@gmail.com>
2022-07-22 10:42:30 +0900

    Lab10 modified : what if 'index' -> 'ptr' ?

--- Lab10/todo_list.h
+++ Lab10/todo_list.h
@@ -1,24 +1,19 @@
 #ifndef TODO_LIST_H
 #define TODO_LIST_H
 
-#include <limits.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <stdbool.h>
-
-typedef int index_t;
 
 typedef struct node {
     int32_t priority;
     char* task;
-    index_t next;
-    index_t d_next;
+    struct node* next;
+    struct node* d_next;
 } node_t;
 
 typedef struct todo_list {
-    node_t* node;
-    index_t head;
-    index_t deleted;
+    node_t* head;
+    node_t* del;
     int dummy;
 } todo_list_t;
 
@@ -26,13 +21,13 @@ todo_list_t init_todo_list(size_t max_size);
 
 void finalize_todo_list(todo_list_t* todo_list);
 
-index_t get_index_for_add(todo_list_t* todo_list, const int32_t priority);
+node_t* get_node_for_add_or_null(todo_list_t* todo_list, const int32_t priority);
 
-void set_node_for_add(node_t* node, const int32_t priority, const char* task, index_t next);
+void set_node_for_add_or_null(node_t* node, const int32_t priority, const char* task, node_t* next);
 
 bool add_todo(todo_list_t* todo_list, const int32_t priority, const char* task);
 
-void delete_index_for_complete(todo_list_t* todo_list, index_t index);
+void delete_node_for_add(todo_list_t* todo_list, node_t* node);
 
 bool complete_todo(todo_list_t* todo_list);
 
